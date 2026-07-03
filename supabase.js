1e95562285e81e3ae2e42169d34a5151c430de66
Randolph329 <chefmasterq81@gmail.com>
2026-07-03 19:37:37 -0400

    Update Supabase key in supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://klcllvuzbhrauckxpoqw.supabase.co";
-const SUPABASE_KEY = "YOUR_PUBLISHABLE_KEY_HERE";
+const SUPABASE_KEY = "sb_publishable_R8xpN6pCOPwupTvGA2tQ8w_vUtPgN65";
 
 const supabaseClient = supabase.createClient(
   SUPABASE_URL,
